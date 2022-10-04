185b01a854e582b0a0b2a495c37dcd0ac5961021
Boldizsár Rácz <boldizsar.racz@flexiana.com>
2022-10-04 19:28:54 +0200

    Add docstrings, do cleanups

--- src/tdd_project/spiral.clj
+++ src/tdd_project/spiral.clj
@@ -1,6 +1,9 @@
 (ns tdd-project.spiral)
 
+
 (defn modify-center
+  "Increase the numbers in m such that it will be
+  the center of a larger spiral matrix."
   [m]
   (let [n (count (first m))
         diff (* 4 (inc n))]
@@ -8,7 +11,11 @@
            (map #(+ % diff) row))
          m)))
 
+
 (defn prepare-wrapping
+  "The wrapping consists of the values that must be
+  added around the modified center to get the larger
+  matrix."
   [n]
   (let [next-n (+ n 2)]
     {:first-row (range 1 (inc next-n))
@@ -21,29 +28,59 @@
                     (range (dec (* 3 next-n))
                            (- (* 4 next-n) 3)))}))
 
+
 (defn wrap-spiral
+  "Produces a larger spiral matrix by wrapping m with
+  the given wrapping-components."
   [m {:keys [first-row
              last-row
              right-column
              left-column] :as _wrapping-components}]
-  (concat
-    [first-row]
-    (map
-      (fn [row left right]
-        (concat [left] row [right]))
-      m
-      left-column
-      right-column)
-    [last-row]))
+  (let [middle-rows
+        (map (fn [row left right]
+               (concat [left] row [right]))
+             m
+             left-column
+             right-column)]
+    (concat
+      [first-row]
+      middle-rows
+      [last-row])))
+
 
 (defn expand-spiral
+  "Expands the spiral matrix m."
   [m]
   (let [n (count (first m))
         modified-center (modify-center m)
         wrapping-components (prepare-wrapping n)]
     (wrap-spiral modified-center wrapping-components)))
 
+
 (defn spiral
+  "Constructs a square matrix with a size N × N containing
+  integers from 1 to N * N in a spiral order, starting from
+  top-left and in clockwise direction.
+
+  # Example
+
+  For n = 3, the output should be
+
+  solution(n) =
+  [[1, 2, 3],
+  [8, 9, 4],
+  [7, 6, 5]]
+
+  # Input/Output
+
+  [input] integer n
+
+  Matrix size, a positive integer.
+
+  Guaranteed constraints:
+  0 ≤ n ≤ 100.
+
+  [output] array.array.integer"
   ([n]
    (spiral (dec n) (if (odd? n) [[1]] [])))
   ([n m]
